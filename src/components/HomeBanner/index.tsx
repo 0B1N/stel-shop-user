0be256727f6c4d887e3d4e2236ba754d6d92397d
김영빈 <youngbin@gim-yeongbins-MacBook-Pro.local>
2024-11-19 07:26:14 +0900

    홈 배너 반응형 처리

--- src/components/HomeBanner/index.tsx
+++ src/components/HomeBanner/index.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import media from "utils/styles/mediaQuery";
 
 type HomeBannerProps = {
   className?: string;
@@ -12,4 +13,8 @@ export default styled(HomeBanner)`
   width: 100%;
   height: 220px;
   background-color: #f4f4f4;
+
+  ${media.small} {
+    height: 338px;
+  }
 `;
