54306f405bfab4fd3d1a9b3b3186649e0f7fc6ff
0B1N <binnyy01@gmail.com>
2024-11-28 12:43:39 +0900

    next config 수정

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
-import type { NextConfig } from "next";
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: "export",
 
-const nextConfig: NextConfig = {
-  pageExtensions: ["mdx", "jsx", "js", "ts", "tsx"],
-  exportPathMap: async function () {
-    return {
-      "/": { page: "/" },
-    };
-  },
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 };
 
 export default nextConfig;
