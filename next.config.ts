fc744c070a075fe5627a2984dab3fbf824934d58
0B1N <binnyy01@gmail.com>
2024-11-28 12:50:38 +0900

    next config 수정

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
-  output: "export",
 
+import type { NextConfig } from "next";
+const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
 
